0eceea7dbf5b78accea69e5a2ba5eac557587ae5
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2024-08-11 18:20:48 +0300

    login done

--- authenticationdemoapp/views.py
+++ authenticationdemoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 # from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,authenticate
 from authenticationdemoapp.forms import SignUpForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -24,4 +25,18 @@ def Home(request):
     return render(request,'athenticationdemoapp/home.html')
 
 def Login(request):
-    return render(request,'athenticationdemoapp/login.html')
+    if request.method =='POST':
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            # get data from form 
+           userName =  form.cleaned_data.get('username')
+           password = form.cleaned_data.get('password')
+           # check user in database
+           user = authenticate(request,username=userName,password=password) 
+           print(user)
+           if user is not None:
+                login(request,user)
+           return redirect('HomePage') 
+    else:
+        form = AuthenticationForm()
+    return render(request,'athenticationdemoapp/login.html',{'form':form})
