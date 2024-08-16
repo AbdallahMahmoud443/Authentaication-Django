32507ab5fb2b80e7a5efe1fb4378c3f498da2bcb
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2024-08-16 16:26:16 +0300

    create custom permissions

--- authenticationdemoapp/models.py
+++ authenticationdemoapp/models.py
@@ -1,5 +1,5 @@
-#from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth.models import AbstractUser,Group,Permission
 # Create your models here.
 
 
@@ -13,4 +13,22 @@
 #       country = models.CharField(choices=COUNTRY_CHOICES,max_length=2)
 #       address = models.CharField(max_length=30,blank=True)
 
-  
+
+class CustomPermissions(models.Model):
+    #! Create one to one between CustomPermissions and Permission type is important
+    permissions = models.OneToOneField(Permission,on_delete=models.CASCADE,primary_key=True)
+    
+    def __str__(self):
+        return self.permissions.name
+    
+    class Meta:
+        permissions =(
+            ('add_staff','can add stuff user'), # (codeName,name)
+            ('update_staff','Can edit stuff user'),
+            ('view_staff','can view stuff users'),
+            ('delete_staff','can delete stuff users'),
+        )
+    
+    
+    
+    
